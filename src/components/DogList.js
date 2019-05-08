de5d501d1a9f832e80a65ceee196f7dee4db2df9
Lauren Hodges <laurenquinn5924@gmail.com>
2019-05-08 10:51:45 -0700

    Added more information to dog breeds

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -22,22 +22,132 @@ class DogList extends React.Component {
 		return(
 			<div className='center_all_breeds'>
 				<div className="filter_by_class">
-						{
-							<ul className="unbullet_list">
-								{breedInfo.filter(dog => 
-									dog.class === 'Working'
-								).map(dog => (
-									<DogBreed 
-									breedName={dog.breedName}
-									colors={dog.colors}
-									image={dog.image}
-									alt={dog.alt}		
-									class={dog.class}						
-								/>
-								))}
-							</ul>
-						}
-					</div>
+					<h2>Working Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Working'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Toy Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Toy'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Terrier Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Terrier'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Sporting Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Sporting'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Non-Sporting Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Non-Sporting'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Hound Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Hound'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+				<div className="filter_by_class">
+					<h2>Herding Class</h2>
+					{
+						<ul className="unbullet_list">
+							{breedInfo.filter(dog => 
+								dog.class === 'Herding'
+							).map(dog => (
+								<DogBreed 
+								breedName={dog.breedName}
+								colors={dog.colors}
+								image={dog.image}
+								alt={dog.alt}		
+								class={dog.class}						
+							/>
+							))}
+						</ul>
+					}
+				</div>
+					
 				<div className='title' >{headerTitle}</div>
 					{
 						<ul className="unbullet_list" >
--- src/components/dogs.js
+++ src/components/dogs.js
@@ -41,7 +41,8 @@ class Dogs extends React.Component {
 					alt: 'Black and Tan Tibetan Mastiff',
 					lifeExpectancy: '12-15 years',
 					weightFemale: '75-120 lbs',
-					weightMale: '100-160 lbs'
+					weightMale: '100-160 lbs',
+					fact: 'In Tibet, they are called “Do-Khyi”(Tied Dog) as they are traditionally kept chained to gates during the day and let loose at night to patrol the property.'
 				},
 				{
 					id: 2,
@@ -50,7 +51,11 @@ class Dogs extends React.Component {
 					class: 'Working',
 					colors: ['Black', 'Black & White', 'Blue', 'Brindle', 'Fawn', 'Harlequin', 'Mantle', 'Merle', 'White'],
 					image: greatDane,
-					alt: 'Merle Great Dane'
+					alt: 'Merle Great Dane',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '75-120 lbs',
+					weightMale: '100-160 lbs',
+					fact: 'The record holder for the tallest dog ever is a Great Dane called Zeus, that measured 42 inches from paw to shoulder.'
 				},
 				{
 					id: 3,
@@ -59,7 +64,11 @@ class Dogs extends React.Component {
 					class: 'Toy',
 					colors: ['Blenheim', 'Black & Tan', 'Tri-Color', 'Ruby'],
 					image: cavalier,
-					alt: 'Tri-Color Cavalier King Charles Spaniel'
+					alt: 'Tri-Color Cavalier King Charles Spaniel',
+					lifeExpectancy: '9-14 years',
+					weightFemale: '13-18 lbs',
+					weightMale: '13-18 lbs',
+					fact: 'Cavaliers are classified in the Toy Group, rather than the Sporting Group with the other spaniels, because they were bred with the express purpose of being a lap dog.'
 				},
 				{
 					id: 4,
@@ -68,8 +77,11 @@ class Dogs extends React.Component {
 					class: 'Toy',
 					colors: ['Black', 'Blue', 'Blue Fawn', 'Cream', 'Fawn', 'Red', 'Red Fawn', 'Sable', 'Seal'],
 					image: italianGrayhound,
-					alt: 'Fawn Italian Grayhound'
-					
+					alt: 'Fawn Italian Grayhound',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '8-11 lbs',
+					weightMale: '8-11 lbs',
+					fact: 'The Italian Greyhound is the smallest of the sighthounds; in fact, it is referred to as “miniature” in size.'
 				},
 				{
 					id: 5,
@@ -78,16 +90,24 @@ class Dogs extends React.Component {
 					class: 'Toy',
 					colors: ['White & Black', 'White & Lemon', 'White & Red', 'White & Sable', 'White Black & Tan'],
 					image: papillon,
-					alt: 'White and Red Papillon'
+					alt: 'White and Red Papillon',
+					lifeExpectancy: '13-15 years',
+					weightFemale: '7-9 lbs',
+					weightMale: '8-10 lbs',
+					fact: 'The Papillon, also called the Continental Toy Spaniel, is a breed of dog, of the spaniel type. One of the oldest of the toy spaniels, it derives its name from its characteristic butterfly-like look of the long and fringed hair on the ears.'
 				},
 				{
 					id: 6,
 					key: 'dogBreed',
-					breedName: 'Poodle',
+					breedName: 'Standard Poodle',
 					class: 'Non-Sporting',
 					colors: ['Apricot', 'Black', 'Blue', 'Brown', 'Cream', 'Gray', 'Red', 'Silver', 'Silver Beige', 'White'],
 					image: poodle,
-					alt: 'Three colors of Standard Poodle. From left: Cream, Red, Gray'
+					alt: 'Three colors of Standard Poodle. From left: Cream, Red, Gray',
+					lifeExpectancy: '10-18 years',
+					weightFemale: '40-50 lbs',
+					weightMale: '60-70 lbs',
+					fact: 'Poodles come in three size varieties: Standards should be more than 15 inches tall at the shoulder; Miniatures are 15 inches or under; Toys stand no more than 10 inches. All three varieties have the same build and proportions.'
 				},
 				{
 					id: 7,
@@ -96,7 +116,11 @@ class Dogs extends React.Component {
 					class: 'Non-Sporting',
 					colors: ['White & Black', 'White & Liver Brown'],
 					image: dalmation,
-					alt: 'White and Liver Brown Dalmation. This dog has brown instead of the popular black spots.'
+					alt: 'White and Liver Brown Dalmation. This dog has brown instead of the popular black spots.',
+					lifeExpectancy: '10-13 years',
+					weightFemale: '35-53 lbs',
+					weightMale: '33-71 lbs',
+					fact: 'Dalmatian puppies are born white and their first spots usually develop within 3 to 4 weeks.'
 				},
 				{
 					id: 8,
@@ -105,7 +129,11 @@ class Dogs extends React.Component {
 					class: 'Herding',
 					colors: ['Tri-Color', 'Blue Merle', 'Red', 'Red Merle'],
 					image: australianShepherd,
-					alt: 'Red Merle Australian Shepherd'
+					alt: 'Red Merle Australian Shepherd',
+					lifeExpectancy: '13-15 years',
+					weightFemale: '35-55 lbs',
+					weightMale: '55-70 lbs',
+					fact: 'In spite of its name, the Australian Shepherd (familiarly known as the Aussie) does not actually originate from Australia. The breed was in fact developed on ranches in the US in the 19th century for herding purposes.'
 				},
 				{
 					id: 9,
@@ -114,7 +142,11 @@ class Dogs extends React.Component {
 					class: 'Herding',
 					colors: ['Bi-Color', 'Black', 'Black & Cream', 'Black & Red', 'Black & Silver', 'Black & Tan', 'Blue', 'Gray', 'Liver', 'Sable', 'White'],
 					image: germanShepherd,
-					alt: 'Sable Long Haired German Shepherd'
+					alt: 'Sable Long Haired German Shepherd',
+					lifeExpectancy: '9-13 years',
+					weightFemale: '49-71 lbs',
+					weightMale: '66-88 lbs',
+					fact: 'Police will only use German Shepherds from the European Working lines. Unlike the American lines, these dogs are structurally sound with straight backs. The American lines have been bred with sloping backs with hips that cannot support them.'
 				},
 				{
 					id: 10,
@@ -123,7 +155,11 @@ class Dogs extends React.Component {
 					class: 'Herding',
 					colors: ['Black & White', 'Blue Merle & White', 'Brindle & White', 'Red & White', 'Sable & White'],
 					image: cardiganWelshCorgi,
-					alt: 'Blue Merle and White Cardigan Welsh Corgi'
+					alt: 'Blue Merle and White Cardigan Welsh Corgi',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '24-34 lbs',
+					weightMale: '30-38 lbs',
+					fact: 'The Cardigan is best described as a Corgi with a tail, but he stands out from his cousin, the Pembroke Welsh Corgi, in other ways. The Cardigan has larger, more rounded ears and he comes in a variety of colors, including tricolor (black and white, with tan or brindle points), blue merle, brindle, sable, and red.'
 				},
 				{
 					id: 11,
@@ -132,7 +168,11 @@ class Dogs extends React.Component {
 					class: 'Hound',
 					colors: ['Black & White', 'Blue Brown & White', 'Black Tan & White', 'Black White & Brown', 'Black White & Tan', 'Brown Black & White', 'Lemon & White', 'Mahogany & White', 'Red & White'],
 					image: bassetHound,
-					alt: 'Brown Black and White Basset Hound'
+					alt: 'Brown Black and White Basset Hound',
+					lifeExpectancy: '10-12 years',
+					weightFemale: '44-60 lbs',
+					weightMale: '51-64 lbs',
+					fact: 'The Basset Hound is a short-legged breed of dog in the hound family. The Basset is a scent hound that was originally bred for the purpose of hunting hare. Their sense of smell and ability to ground-scent is second only to the Bloodhound.'
 				},
 				{
 					id: 12,
@@ -141,7 +181,11 @@ class Dogs extends React.Component {
 					class: 'Hound',
 					colors: ['Black & Gray', 'Black & Silver', 'Black White & Silver', 'Gray & Black', 'Gray Black & Silver', 'Silver & Black', 'Silver Gray & Black'],
 					image: norwegianElkhound,
-					alt: 'Gray and Black Norwegian Elkhound'
+					alt: 'Gray and Black Norwegian Elkhound',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '49-55 lbs',
+					weightMale: '49-55 lbs',
+					fact: 'Records indicate the Norwegian Elkhound could have emerged as early as 5,000 B.C. Archeologists have found skeletons resembling the Norwegian Elkhound in the same sites where they have found Viking remains and weapons. This would make them one of the oldest dog breeds in the world.'
 				},
 				{
 					id: 13,
@@ -150,7 +194,11 @@ class Dogs extends React.Component {
 					class: 'Sporting',
 					colors: ['Dark Golden', 'Golden', 'Light Golden'],
 					image: goldenRetriever,
-					alt: 'Light Golden Retriever'
+					alt: 'Light Golden Retriever',
+					lifeExpectancy: '10-12 years',
+					weightFemale: '55-71 lbs',
+					weightMale: '65-75 lbs',
+					fact: 'Golden Retrievers in the U.S. have a high rate of cancer compared to many other breeds.The high rate of cancer in Goldens is a relatively recent development. Approximately 60% of Goldens will die of cancer.'
 				},
 				{
 					id: 14,
@@ -159,7 +207,11 @@ class Dogs extends React.Component {
 					class: 'Sporting',
 					colors: ['Red', 'Red Gold'],
 					image: toller,
-					alt: 'Nova Scotia Duck Tolling Retriever'
+					alt: 'Nova Scotia Duck Tolling Retriever',
+					lifeExpectancy: '10-14 years',
+					weightFemale: '37-44 lbs',
+					weightMale: '44-51 lbs',
+					fact: 'Tollers were created to employ an ingenious hunting method. They imitate the curious activity of foxes, whose color and quick movements exert a strange fascination over waterfowl. The sight of a Toller playing fetch along the shoreline arouses the curiosity of ducks offshore.'
 				},
 				{
 					id: 15,
@@ -168,7 +220,11 @@ class Dogs extends React.Component {
 					class: 'Sporting',
 					colors: ['Black', 'Black & Tan', 'Black & White', 'Black White & Tan', 'Blue Roan', 'Blue Roan & Tan', 'Golden', 'Lemon Roan', 'Liver', 'Liver & Tan', 'Liver & White', 'Liver Roan', 'Liver Roan & Tan', 'Liver White & Tan', 'Orange & White', 'Orange Roan', 'Red', 'Red Roan'],
 					image: cocker,
-					alt: 'Liver Roan English Cocker Spaniel'
+					alt: 'Liver Roan English Cocker Spaniel',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '26-33 lbs',
+					weightMale: '29-35 lbs',
+					fact: 'These small spaniels were originally bred to hunt birds. They were especially skilled at hunting a type of bird called the woodcock. This is where the Cocker Spaniel\'s name comes from.'
 				},
 				{
 					id: 16,
@@ -177,16 +233,24 @@ class Dogs extends React.Component {
 					class: 'Terrier',
 					colors: ['Black', 'Black Brindle', 'Brindle', 'Cream', 'Cream Brindle', 'Gray', 'Gray Brindle', 'Red', 'Red Brindle', 'Silver', 'Wheaten'],
 					image: cairnTerrier,
-					alt: 'Red Brindle Cairn Terrier'
+					alt: 'Red Brindle Cairn Terrier',
+					lifeExpectancy: '12-15 years',
+					weightFemale: '13-18 lbs',
+					weightMale: '13-18 lbs',
+					fact: 'Cairn Terriers have front paws are well-padded and larger than their rear ones, a trait that gave them an advantage when digging in their pursuit of rodents.'
 				},
 				{
 					id: 17,
 					key: 'dogBreed',
-					breedName: 'Russell Terrier',
+					breedName: 'Jack Russell Terrier',
 					class: 'Terrier',
 					colors: ['White'],
 					image: russellTerrier,
-					alt: 'White Russell Terrier with Tan Markings'
+					alt: 'White Russell Terrier with Tan Markings',
+					lifeExpectancy: '13-16 years',
+					weightFemale: '14-18 lbs',
+					weightMale: '14-18 lbs',
+					fact: 'The JRT can leap five times its own height. That means a 12-inch-tall Jack can easily jump five feet in the air.'
 				},
 				{
 					id: 18,
@@ -195,7 +259,11 @@ class Dogs extends React.Component {
 					class: 'Working',
 					colors: ['Black Rust & White', 'Black Tan & White'],
 					image: berner,
-					alt: 'Bernese Mountain Dog'
+					alt: 'Bernese Mountain Dog',
+					lifeExpectancy: '6-8 years',
+					weightFemale: '79-110 lbs',
+					weightMale: '84-110 lbs',
+					fact: 'Berners have numerous health problems due to their small genetic foundation, and perhaps due to other reasons yet undiscovered. Currently, the life span of a Bernese Mountain Dog is comparatively short, about six to eight years, though in recent years, the life expectancy has gone up to about ten years. This may be due to more responsible breeding and regard for genetic conditions.'
 				},
 				{
 					id: 19,
@@ -204,7 +272,11 @@ class Dogs extends React.Component {
 					class: 'Working',
 					colors: ['Black', 'Brown', 'Gray', 'White & Black'],
 					image: newfie,
-					alt: 'Two Newfoundland Dogs showing off their black and brown coats.'
+					alt: 'Two Newfoundland Dogs showing off their black and brown coats.',
+					lifeExpectancy: '8-10 years',
+					weightFemale: '99-120 lbs',
+					weightMale: '130-150 lbs',
+					fact: 'Newfoundlands have a knack for water rescue and a natural inclination towards rescuing struggling or drowning people from water. They have been known to brave treacherous water and dangerous conditions to pull people to safety.'
 				},
 				{
 					id: 20,
@@ -213,7 +285,11 @@ class Dogs extends React.Component {
 					class: 'Working',
 					colors: ['Black & Rust', 'Red & Rust'],
 					image: dobie,
-					alt: 'Red and Rust Doberman'
+					alt: 'Red and Rust Doberman',
+					lifeExpectancy: '10-13 years',
+					weightFemale: '71-77 lbs',
+					weightMale: '88-99 lbs',
+					fact: 'The Doberman was developed by Karl Louis Dobermann. Dobermann was a tax collector and found himself walking with large sums of money at night in dangerous parts of town. He created the Doberman to be intimidating but refined.'
 
 				},
 				{
@@ -224,9 +300,10 @@ class Dogs extends React.Component {
 					colors: ['Black & Tan', 'Cream', 'Red', 'Red Sesame'],
 					image: shiba,
 					alt: 'Red Sesame Shiba Inu',
-					lifeExpectancy: '13-16 years',
+					lifeExpectancy: '12-15 years',
 					weightFemale: '15-20 lbs',
-					weightMale: '18-24 lbs'
+					weightMale: '18-24 lbs',
+					fact: 'These tenacious hunters were bred to go after boars, birds, and bears. They also have dense undercoats, which allow them to withstand cold temperatures—a particularly helpful trait for a hunting dog.'
 				}
 			]
 		}
